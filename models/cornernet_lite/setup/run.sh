d542fa8c524fd809da2b1061df9a333ee2bdef87
Trever Anderson <treveranderson01@gmail.com>
2019-09-15 13:50:08 -0500

    creating repo dir in setup script

--- models/cornernet_lite/setup/run.sh
+++ models/cornernet_lite/setup/run.sh
@@ -1,6 +1,9 @@
-if [ ! -d "repo/CornerNet-Lite" ]; then
-  git clone git@github.com:princeton-vl/CornerNet-Lite.git repo/CornerNet-Lite
+if [ ! -d "../repo" ]; then
+  mkdir ../repo
 fi
-if [ ! -d "repo/coco" ]; then
-  git clone git@github.com:cocodataset/cocoapi.git repo/coco
+if [ ! -d "../repo/CornerNet-Lite" ]; then
+  git clone git@github.com:princeton-vl/CornerNet-Lite.git ../repo/CornerNet-Lite
+fi
+if [ ! -d "../repo/coco" ]; then
+  git clone git@github.com:cocodataset/cocoapi.git ../repo/coco
 fi
