a294dd888e1a719490212b7297c9e5229fe62837
Trever Anderson <treveranderson01@gmail.com>
2019-10-25 21:48:03 -0500

    can create cornernet model. must use conda environment and compile things before

--- models/cornernet/fps.py
+++ models/cornernet/fps.py
@@ -1,3 +1,4 @@
+import torch
 import argparse
 import os.path as path
 import sys
@@ -5,6 +6,8 @@ import json
 import os
 import shutil
 import importlib
+import net_funcs as nf
+import time
 
 CURRENT_DIR = './'
 MODEL = 'CornerNet'
@@ -13,16 +16,19 @@ sys.path.append(path.abspath(MODEL_PATH))
 from config import system_configs
 from db.datasets import datasets
 
+from nnet.py_factory import NetworkFactory
+
 SPLIT = 'testing'
 DB = 'db'
 IMAGE_SIZE = 300
 IMAGE_NAME_FILE = 'top_600.txt'
-TRAINED_MODEL_DIR = 'cornernet/weights/'
+TRAINED_MODEL_DIR = './weights'
 TRAINED_MODEL_FN = 'CornerNet_500000.pkl'
 TRAINED_MODEL_PATH = path.join(TRAINED_MODEL_DIR, TRAINED_MODEL_FN)
 TEST_DEV_ANNOS = 'data/coco/annotations/'
 TEST_DEV_ANNOS_FN = 'instances_testdev2017.json'
 TEST_DEV_PATH = path.join(CURRENT_DIR, path.join(TEST_DEV_ANNOS, TEST_DEV_ANNOS_FN))
+CACHE_PATH = './cache/nnet/CornerNet'
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Measure FPS of CornerNet')
@@ -47,34 +53,43 @@ def read_json_file(path):
     return data
 
 def make_dirs(dirs):
-    list(map(lambda d: os.makedirs(d), 
+    list(map(lambda d: os.makedirs(d, exist_ok=True), 
              list(filter(lambda d: not path.exists(d), dirs))))
 
-def run_model(data, system_configs, result_dir):
-    corner_net = NetworkFactory(data)
+def create_net(dataset):
+    corner_net = NetworkFactory(dataset)
     corner_net.load_params(system_configs.max_iter)
-    test_file = "test.{}".format(data.data)
-    testing = importlib.import_module(test_file).testing
-
     corner_net.cuda()
     corner_net.eval_mode()
-    
+
+    return corner_net
+
+def inference(dataset, nnet, image):
+    start = time.time()
+    nf.inference(dataset=dataset, nnet=nnet, image_file=image)
+    end = time.time()
+
+    return (end - start) * 1000
+
+def test_model():
+    pass
 
 if __name__ == '__main__':
     args = parse_args()
 
     #prepare dirs
-    make_dirs([path.join(CURRENT_DIR, TEST_DEV_ANNOS)])
+    make_dirs([path.join(CURRENT_DIR, TEST_DEV_ANNOS), CACHE_PATH])
     shutil.copy(path.join(CURRENT_DIR, TEST_DEV_ANNOS_FN), TEST_DEV_PATH)
+    shutil.copy(TRAINED_MODEL_PATH, CACHE_PATH)
 
     model_config_path = args.model_config + args.suffix
     config_fp = path.join(MODEL_PATH, path.join(system_configs.config_dir, model_config_path))
     config = read_json_file(path=config_fp)
     config['system']['snapshot_name'] = args.model_config
     system_configs.update_config(config['system'])
-    
-    test_dataset = datasets[system_configs.dataset](config[DB], system_configs.test_split)
 
+    test_dataset = datasets[system_configs.dataset](config[DB], system_configs.test_split)
+    net = create_net(dataset=test_dataset)
     
-    print(test_dataset._cat_ids)
+    #inference(dataset=test_dataset, nnet=net, )
     
--- models/cornernet/net_funcs.py
+++ models/cornernet/net_funcs.py
@@ -1,117 +1,107 @@
+import torch
 import numpy as np
 import os
 import cv2
+import sys
+
+sys.path.append('./CornerNet')
 from external.nms import soft_nms, soft_nms_merge
 
-def inference(db, nnet, result_dir, debug=False, decode_func=kp_decode):
-    debug_dir = os.path.join(result_dir, "debug")
-    if not os.path.exists(debug_dir):
-        os.makedirs(debug_dir)
+def kp_decode(nnet, images, K, ae_threshold=0.5, kernel=3):
+    detections = nnet.test([images], ae_threshold=ae_threshold, K=K, kernel=kernel)
+    detections = detections.data.cpu().numpy()
+    return detections
 
-    if db.split != "trainval":
-        db_inds = db.db_inds[:100] if debug else db.db_inds
-    else:
-        db_inds = db.db_inds[:100] if debug else db.db_inds[:5000]
-    num_images = db_inds.size
-
-    K             = db.configs["top_k"]
-    ae_threshold  = db.configs["ae_threshold"]
-    nms_kernel    = db.configs["nms_kernel"]
-    
-    scales        = db.configs["test_scales"]
-    weight_exp    = db.configs["weight_exp"]
-    merge_bbox    = db.configs["merge_bbox"]
-    categories    = db.configs["categories"]
-    nms_threshold = db.configs["nms_threshold"]
-    max_per_image = db.configs["max_per_image"]
+def inference(dataset, nnet, image_file, decode_func=kp_decode):
+    K = dataset.configs["top_k"]
+    ae_threshold = dataset.configs["ae_threshold"]
+    nms_kernel = dataset.configs["nms_kernel"]
+    scales = dataset.configs["test_scales"]
+    weight_exp = dataset.configs["weight_exp"]
+    merge_bbox = dataset.configs["merge_bbox"]
+    categories = dataset.configs["categories"]
+    nms_threshold = dataset.configs["nms_threshold"]
+    max_per_image = dataset.configs["max_per_image"]
     nms_algorithm = {
         "nms": 0,
         "linear_soft_nms": 1, 
         "exp_soft_nms": 2
-    }[db.configs["nms_algorithm"]]
+    }[dataset.configs["nms_algorithm"]]
 
     top_bboxes = {}
-    for ind in tqdm(range(0, num_images), ncols=80, desc="locating kps"):
-        db_ind = db_inds[ind]
+    image = cv2.imread(image_file)
+    height, width = image.shape[0:2]
+    detections = []
+    for scale in scales:
+        new_height = int(height * scale)
+        new_width  = int(width * scale)
+        new_center = np.array([new_height // 2, new_width // 2])
 
-        image_id   = db.image_ids(db_ind)
-        image_file = db.image_file(db_ind)
-        image      = cv2.imread(image_file)
+        inp_height = new_height | 127
+        inp_width  = new_width  | 127
 
-        height, width = image.shape[0:2]
+        images  = np.zeros((1, 3, inp_height, inp_width), dtype=np.float32)
+        ratios  = np.zeros((1, 2), dtype=np.float32)
+        borders = np.zeros((1, 4), dtype=np.float32)
+        sizes   = np.zeros((1, 2), dtype=np.float32)
 
-        detections = []
+        out_height, out_width = (inp_height + 1) // 4, (inp_width + 1) // 4
+        height_ratio = out_height / inp_height
+        width_ratio  = out_width  / inp_width
 
-        for scale in scales:
-            new_height = int(height * scale)
-            new_width  = int(width * scale)
-            new_center = np.array([new_height // 2, new_width // 2])
+        resized_image = cv2.resize(image, (new_width, new_height))
+        resized_image, border, offset = crop_image(resized_image, new_center, 
+                                                   [inp_height, inp_width])
 
-            inp_height = new_height | 127
-            inp_width  = new_width  | 127
+        resized_image = resized_image / 255.
+        normalize_(resized_image, dataset.mean, dataset.std)
 
-            images  = np.zeros((1, 3, inp_height, inp_width), dtype=np.float32)
-            ratios  = np.zeros((1, 2), dtype=np.float32)
-            borders = np.zeros((1, 4), dtype=np.float32)
-            sizes   = np.zeros((1, 2), dtype=np.float32)
+        images[0]  = resized_image.transpose((2, 0, 1))
+        borders[0] = border
+        sizes[0]   = [int(height * scale), int(width * scale)]
+        ratios[0]  = [height_ratio, width_ratio]
 
-            out_height, out_width = (inp_height + 1) // 4, (inp_width + 1) // 4
-            height_ratio = out_height / inp_height
-            width_ratio  = out_width  / inp_width
+        images = np.concatenate((images, images[:, :, :, ::-1]), axis=0)
+        images = torch.from_numpy(images)
+        dets   = decode_func(nnet, images, K, ae_threshold=ae_threshold, kernel=nms_kernel)
+        dets   = dets.reshape(2, -1, 8)
+        dets[1, :, [0, 2]] = out_width - dets[1, :, [2, 0]]
+        dets   = dets.reshape(1, -1, 8)
 
-            resized_image = cv2.resize(image, (new_width, new_height))
-            resized_image, border, offset = crop_image(resized_image, new_center, 
-                                                       [inp_height, inp_width])
+        _rescale_dets(dets, ratios, borders, sizes)
+        dets[:, :, 0:4] /= scale
+        detections.append(dets)
 
-            resized_image = resized_image / 255.
-            normalize_(resized_image, db.mean, db.std)
+    detections = np.concatenate(detections, axis=1)
 
-            images[0]  = resized_image.transpose((2, 0, 1))
-            borders[0] = border
-            sizes[0]   = [int(height * scale), int(width * scale)]
-            ratios[0]  = [height_ratio, width_ratio]
+    classes    = detections[..., -1]
+    classes    = classes[0]
+    detections = detections[0]
 
-            images = np.concatenate((images, images[:, :, :, ::-1]), axis=0)
-            images = torch.from_numpy(images)
-            dets   = decode_func(nnet, images, K, ae_threshold=ae_threshold, kernel=nms_kernel)
-            dets   = dets.reshape(2, -1, 8)
-            dets[1, :, [0, 2]] = out_width - dets[1, :, [2, 0]]
-            dets   = dets.reshape(1, -1, 8)
+    # reject detections with negative scores
+    keep_inds  = (detections[:, 4] > -1)
+    detections = detections[keep_inds]
+    classes    = classes[keep_inds]
 
-            _rescale_dets(dets, ratios, borders, sizes)
-            dets[:, :, 0:4] /= scale
-            detections.append(dets)
+    top_bboxes[image_id] = {}
+    for j in range(categories):
+        keep_inds = (classes == j)
+        top_bboxes[image_id][j + 1] = detections[keep_inds][:, 0:7].astype(np.float32)
+        if merge_bbox:
+            soft_nms_merge(top_bboxes[image_id][j + 1], Nt=nms_threshold, 
+                           method=nms_algorithm, weight_exp=weight_exp)
+        else:
+            soft_nms(top_bboxes[image_id][j + 1], Nt=nms_threshold, method=nms_algorithm)
+        top_bboxes[image_id][j + 1] = top_bboxes[image_id][j + 1][:, 0:5]
 
-        detections = np.concatenate(detections, axis=1)
-
-        classes    = detections[..., -1]
-        classes    = classes[0]
-        detections = detections[0]
-
-        # reject detections with negative scores
-        keep_inds  = (detections[:, 4] > -1)
-        detections = detections[keep_inds]
-        classes    = classes[keep_inds]
-
-        top_bboxes[image_id] = {}
-        for j in range(categories):
-            keep_inds = (classes == j)
-            top_bboxes[image_id][j + 1] = detections[keep_inds][:, 0:7].astype(np.float32)
-            if merge_bbox:
-                soft_nms_merge(top_bboxes[image_id][j + 1], Nt=nms_threshold, 
-                               method=nms_algorithm, weight_exp=weight_exp)
-            else:
-                soft_nms(top_bboxes[image_id][j + 1], Nt=nms_threshold, method=nms_algorithm)
-            top_bboxes[image_id][j + 1] = top_bboxes[image_id][j + 1][:, 0:5]
-
-        scores = np.hstack([
-            top_bboxes[image_id][j][:, -1] 
-            for j in range(1, categories + 1)
-        ])
-        if len(scores) > max_per_image:
-            kth    = len(scores) - max_per_image
-            thresh = np.partition(scores, kth)[kth]
-            for j in range(1, categories + 1):
-                keep_inds = (top_bboxes[image_id][j][:, -1] >= thresh)
-                top_bboxes[image_id][j] = top_bboxes[image_id][j][keep_inds]
-        return top_bboxes
+    scores = np.hstack([
+        top_bboxes[image_id][j][:, -1] 
+        for j in range(1, categories + 1)
+    ])
+    if len(scores) > max_per_image:
+        kth    = len(scores) - max_per_image
+        thresh = np.partition(scores, kth)[kth]
+        for j in range(1, categories + 1):
+            keep_inds = (top_bboxes[image_id][j][:, -1] >= thresh)
+            top_bboxes[image_id][j] = top_bboxes[image_id][j][keep_inds]
+    return top_bboxes
