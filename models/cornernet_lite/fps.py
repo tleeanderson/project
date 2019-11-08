dcb8fb96bc4861dbf5465f6d797f1f8d3fff5fab
Trever Anderson <treveranderson01@gmail.com>
2019-11-07 20:04:38 -0600

    can build cornernet_lite from scratch. can call into models

--- models/cornernet_lite/fps.py
+++ models/cornernet_lite/fps.py
@@ -1,5 +1,9 @@
+import sys
 sys.path.append('..')
 import common
+from CornerNet_Lite import CornerNet
+import time
+import cv2
 
 def parse_args():
     parser = common.default_args(net_name=NET_NAME, trained_model_path=TRAINED_MODEL_PATH, 
@@ -9,4 +13,10 @@ def parse_args():
     return parser.parse_args()
 
 if __name__ == '__main__':
-    
+    img = cv2.imread('/home/tanderson/models/datasets/mscoco/raw-data/val2014/COCO_val2014_000000581632.jpg')
+    net = CornerNet()
+    start = time.time()
+    out = net(img)
+    end = time.time()
+
+    print("output time: " + str(end - start))
--- models/cornernet_lite/setup.sh
+++ models/cornernet_lite/setup.sh
@@ -6,13 +6,13 @@ if [ ! -d $PROJECT_NAME ]; then
   echo "Pulling down $GIT_PROJECT project..."
   git clone git@github.com:princeton-vl/$GIT_PROJECT.git $PROJECT_NAME
 else
-  echo "$PROJECT_NAME exists, nothing to do, exiting"
+  echo "$PROJECT_NAME exists, will not pull from github"
 fi
 
 echo "setting up conda environment"
 conda create --name CornerNet_Lite --file $PROJECT_NAME/conda_packagelist.txt --channel pytorch
 eval "$(conda shell.bash hook)"
-source activate CornerNet_Lite
+conda activate CornerNet_Lite
 
 echo "compiling corner pooling layers"
 cd $PROJECT_NAME/core/models/py_utils/_cpools/
@@ -21,4 +21,31 @@ python setup.py install --user
 echo "Compiling non max suppresion"
 cd -
 cd $PROJECT_NAME/core/external
+echo "current dir " $(pwd)
 make
+
+cd -
+echo "moving model files"
+declare -A DIRS_FILES
+DIRS_FILES[CornerNet_Saccade]=CornerNet_Saccade_500000.pkl
+DIRS_FILES[CornerNet_Squeeze]=CornerNet_Squeeze_500000.pkl
+DIRS_FILES[CornerNet]=CornerNet_500000.pkl
+
+ALL_EXIST="true"
+for d in "${!DIRS_FILES[@]}"; do
+  f=${DIRS_FILES[$d]}
+  if [ ! -f ../../pretrained/$f ]; then
+    echo $f "was not found, please execute tar -zxvf pretrained_models.tar.gz -C . in the project root"
+    ALL_EXIST="false"
+  fi
+done
+
+if [ "$ALL_EXIST" = true ]; then
+  CACHE_PATH=cache/nnet
+  MODEL_DIRS="CornerNet_Saccade CornerNet_Squeeze CornerNet"
+  for d in "${!DIRS_FILES[@]}"; do
+    f=${DIRS_FILES[$d]}
+    mkdir -p $PROJECT_NAME/$CACHE_PATH/$d
+    mv ../../pretrained/$f $PROJECT_NAME/$CACHE_PATH/$d/$f
+  done
+fi
