5cbbd9cb6d2986465e99b353c28ac7d3646b5ae2
Trever Anderson <treveranderson01@gmail.com>
2019-11-15 18:15:27 -0600

    cornernet_lite reads data once, can do batch inference, but impls do not allow

--- models/cornernet_lite/fps.py
+++ models/cornernet_lite/fps.py
@@ -12,6 +12,9 @@ CORNER_NET = 'CornerNet'
 CORNER_NET_SQUEEZE = 'CornerNet_Squeeze'
 CORNER_NET_SACCADE = 'CornerNet_Saccade'
 IMAGE_SIZE = 512
+"""Inference implementations do not support batch inference
+   https://github.com/princeton-vl/CornerNet-Lite/issues/105"""
+BATCH_SIZE = 1
 
 def parse_args():
     parser = common.default_args(net_name=NET_NAME,  num_classes=81, image_size=IMAGE_SIZE)
@@ -19,20 +22,25 @@ def parse_args():
                         choices=[CORNER_NET, CORNER_NET_SACCADE, CORNER_NET_SQUEEZE])
     return parser.parse_args()
 
-def inference(model, image):
+def inference(model, image, batch_size):
     return common.time_inference(inference_func=model, 
-                                 inference_func_args={'image': image})
+                                 inference_func_args={'image': image}, 
+                                 batch_size=batch_size)
 
-def test_model(args, size, model):
-    images = common.read_images(image_name_file=args.image_name_file, 
-                                image_path=args.image_path, size=size)
+def read_data(args, size, batch_size):
+    return common.read_images(image_name_file=args.image_name_file,
+                              image_path=args.image_path, size=size)
+
+def test_model(args, size, model, batch_size, images):
     return common.test_model(im_data=images, inference_func=inference, 
-                             inference_func_args={'model': model})
+                             inference_func_args={'model': model, 
+                                                  'batch_size': batch_size})
 
-def average_averages(args, size, model):
+def average_averages(args, size, model, batch_size, images):
     return common.average_averages(times=args.num_tests, test_model_func=test_model, 
                                 test_model_args={'args': args, 'size': size, 
-                                                 'model': model})
+                                                 'model': model, 'batch_size': batch_size, 
+                                                 'images': images})
 
 def build_model(model_name, cn, cn_sac, cn_sq):
     if model_name == cn:
@@ -46,5 +54,7 @@ if __name__ == '__main__':
     args = parse_args()
     model = build_model(args.model, cn=CORNER_NET, 
                         cn_sac=CORNER_NET_SACCADE, cn_sq=CORNER_NET_SQUEEZE)
-    out = average_averages(args=args, size=args.image_size, model=model)
+    images = read_data(args=args, size=args.image_size, batch_size=BATCH_SIZE)
+    out = average_averages(args=args, size=args.image_size, model=model,
+                           batch_size=BATCH_SIZE, images=images)
     common.print_output(args=args, out_data=out, model_name=args.model)
