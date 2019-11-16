3d6feffa76d1ba0ab3edb8e5b58b52b05431be4a
Trever Anderson <treveranderson01@gmail.com>
2019-11-15 20:26:02 -0600

    cleaned up cornernet_lite and documented

--- models/cornernet_lite/fps.py
+++ models/cornernet_lite/fps.py
@@ -17,44 +17,98 @@ IMAGE_SIZE = 512
 BATCH_SIZE = 1
 
 def parse_args():
+    """Parses arguments by calling up to common and then adding
+       CornerNet_Lite specific arguments.
+
+    Args:
+         None
+    
+    Returns: parsed arguments
+    """
     parser = common.default_args(net_name=NET_NAME,  num_classes=81, image_size=IMAGE_SIZE)
     parser.add_argument('--model', required=False, default=CORNER_NET, 
                         choices=[CORNER_NET, CORNER_NET_SACCADE, CORNER_NET_SQUEEZE])
     return parser.parse_args()
 
 def inference(model, image, batch_size):
+    """Executes common.time_inference function with CornerNet_Lite arguments.
+
+    Args:
+         model: CornerNet_Lite model
+         image: input image
+         batch_size: size of batch
+    
+    Returns: tuple
+    """
     return common.time_inference(inference_func=model, 
                                  inference_func_args={'image': image}, 
                                  batch_size=batch_size)
 
-def read_data(args, size, batch_size):
-    return common.read_images(image_name_file=args.image_name_file,
-                              image_path=args.image_path, size=size)
+def read_data(image_name_file, image_path, size, batch_size):
+    """Executes common.read_images with CornerNet_Lite arguments.
 
-def test_model(args, size, model, batch_size, images):
+    Args:
+         image_name_file: file with names of images
+         image_path: path to images
+         size: size of images
+         batch_size: size of batch
+    
+    Returns: np array of shape (num_images, size, size, channels)
+    """
+    return common.read_images(image_name_file=image_name_file,
+                              image_path=image_path, size=size)
+
+def test_model(size, model, batch_size, images):
+    """Executes common.test_model with CornerNet_Lite arguments.
+
+    Args:
+         size: size of model
+         model: some CornerNet_Lite model
+         batch_size: size of batch
+         images: input images
+    
+    Returns: map
+    """
     return common.test_model(im_data=images, inference_func=inference, 
                              inference_func_args={'model': model, 
                                                   'batch_size': batch_size})
 
-def average_averages(args, size, model, batch_size, images):
+def average_averages(size, model, batch_size, images):
+    """Executes common.average_averages with CornerNet_Lite arguments.
+
+    Args:
+         size: size of images
+         model: some CornerNet_Lite model
+         batch_size: size of batch
+         images: some images
+    
+    Returns: map
+    """
     return common.average_averages(times=args.num_tests, test_model_func=test_model, 
-                                test_model_args={'args': args, 'size': size, 
-                                                 'model': model, 'batch_size': batch_size, 
+                                test_model_args={'size': size, 'model': model, 
+                                                 'batch_size': batch_size, 
                                                  'images': images})
 
-def build_model(model_name, cn, cn_sac, cn_sq):
-    if model_name == cn:
+def build_model(model_name):
+    """Builds a model.
+
+    Args:
+         model_name: name of model
+
+    Returns: model
+    """
+    if model_name == CORNER_NET:
         return CornerNet()
-    elif model_name == cn_sac:
+    elif model_name == CORNER_NET_SACCADE:
         return CornerNet_Saccade()
-    elif model_name == cn_sq:
+    elif model_name == CORNER_NET_SQUEEZE:
         return CornerNet_Squeeze()
 
 if __name__ == '__main__':
     args = parse_args()
-    model = build_model(args.model, cn=CORNER_NET, 
-                        cn_sac=CORNER_NET_SACCADE, cn_sq=CORNER_NET_SQUEEZE)
-    images = read_data(args=args, size=args.image_size, batch_size=BATCH_SIZE)
-    out = average_averages(args=args, size=args.image_size, model=model,
+    model = build_model(args.model)
+    images = read_data(image_name_file=args.image_name_file, image_path=args.image_path, 
+                       size=args.image_size, batch_size=BATCH_SIZE)
+    out = average_averages(size=args.image_size, model=model,
                            batch_size=BATCH_SIZE, images=images)
     common.print_output(num_tests=args.num_tests, out_data=out, model_name=args.model)
